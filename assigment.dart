ed00061457d20df03e8fa76938ab1b85694ab11b
iyked3 <154082030+iyked3@users.noreply.github.com>
2024-03-03 17:02:01 +0100

    Add files via upload

---
+++ assigment.dart
@@ -0,0 +1,25 @@
+void main() {
+  // Integer
+  int age = 21; // Example of an integer representing age
+  print('Age: $age');
+
+  // Double
+  double temperature = 26.5; // Example of a double representing temperature
+  print('Temperature: $temperature');
+
+  // String
+  String name = 'dennis'; // Example of a string representing a person's name
+  print('Name: $name');
+
+  // List
+  List<int> numbers = [1, 2, 3, 4, 5]; // Example of a list of integers
+  print('Numbers: $numbers');
+
+  // Map
+  Map<String, dynamic> person = {
+    'name': 'dennis',
+    'age': 22,
+    'city': 'lagos'
+  }; // Example of a map representing a person
+  print('Person: $person');
+}
